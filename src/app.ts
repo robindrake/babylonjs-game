d6f5979c6de9cfe1705809704e1680278b4e88bc
Robyn Drake <robin.drake.041@gmail.com>
2022-09-21 00:32:01 -0500

    app.ts done, for now

---
+++ src/app.ts
@@ -0,0 +1,50 @@
+import "@babylonjs/core/Debug/debugLayer";
+import "@babylonjs/inspector";
+import "@babylonjs/loaders/glTF";
+import {Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder } from "@babylonjs/core";
+
+class App {
+    constructor() {
+        //set up canvas
+        var canvas = document.createElement("canvas");
+        canvas.style.width = "1000px";
+        canvas.style.height = "1000px";
+        canvas.id = "gameCanvas";
+        document.body.appendChild(canvas);
+
+        //Initialize babylon scene and engine
+        var engine = new Engine(canvas, true);
+        var scene = new Scene(engine);
+        
+        //Set up Camera and Light
+        var camera: ArcRotateCamera = new ArcRotateCamera(
+            "Camera",
+            Math.PI / 2,
+            Math.PI / 2,
+            2,
+            Vector3.Zero(),
+            scene
+        );
+        camera.attachControl(canvas, true);
+
+        var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
+
+        //Hide / show the inspector
+        window.addEventListener("keydown", (ev) => {
+            if(ev.shiftKey && ev.ctrlKey && ev.altKey && ev.keyCode === 72) {
+                if(scene.debugLayer.isVisible()) {
+                    scene.debugLayer.hide();
+                } else {
+                    scene.debugLayer.show();
+                }
+            }
+        });
+
+        //The main render loop
+        engine.runRenderLoop(() => {
+            scene.render();
+        });
+    }
+}
+
+new App();
