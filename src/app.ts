ecbe4dd3f484a03bfd321fbc9a268db254528500
Robyn Drake <robin.drake.041@gmail.com>
2022-09-21 03:20:46 -0500

    interim commit

--- src/app.ts
+++ src/app.ts
@@ -3,18 +3,14 @@ import "@babylonjs/inspector";
 import "@babylonjs/loaders/glTF";
 import {Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder } from "@babylonjs/core";
 
-//enum for states
-enum State { START = 0, GAME = 1, LOSE = 2, CUTSCENE = 3 }
-
 class App {
     // General Entire Application
     private _scene: Scene;
     private _canvas: HTMLCanvasElement;
     private _engine: Engine;
-
-    //Scene - related
-    private _state: number = 0;
-
+    private _light: HemisphericLight;
+    private _box: Mesh;
+    
     constructor() {
         this._canvas = this._createCanvas();
 
@@ -22,22 +18,13 @@ class App {
         this._engine = new Engine(this._canvas, true);
         this._scene = new Scene(this._engine);
 
-        var camera: ArcRotateCamera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), this._scene);
-        camera.attachControl(this._canvas, true);
-        var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(1, 1, 0), this._scene);
-        var sphere: Mesh = MeshBuilder.CreateSphere("sphere", { diameter: 1 }, this._scene);
+        this._light = new HemisphericLight(
+            "light",
+            new Vector3(0, 1, 0),
+            this._scene
+        );
 
-        // hide/show the Inspector
-        window.addEventListener("keydown", (ev) => {
-            // Shift+Ctrl+Alt+I
-            if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.keyCode === 73) {
-                if (this._scene.debugLayer.isVisible()) {
-                    this._scene.debugLayer.hide();
-                } else {
-                    this._scene.debugLayer.show();
-                }
-            }
-        });
+
 
         // run the main render loop
         this._engine.runRenderLoop(() => {
