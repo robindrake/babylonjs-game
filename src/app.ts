29d96ac2dbf2ef919316999cf8de6acaa48c7b65
Robyn Drake <robin.drake.041@gmail.com>
2022-09-21 03:31:15 -0500

    Following https://doc.babylonjs.com/start/chap1/first_scene

--- src/app.ts
+++ src/app.ts
@@ -10,13 +10,33 @@ class App {
     private _engine: Engine;
     private _light: HemisphericLight;
     private _box: Mesh;
+    private _camera: ArcRotateCamera;
     
     constructor() {
         this._canvas = this._createCanvas();
 
         // initialize babylon scene and engine
         this._engine = new Engine(this._canvas, true);
+        this._scene = this._createScene();
+
+        
+
+        // run the main render loop
+        this._engine.runRenderLoop(() => {
+            this._scene.render();
+        });
+    }
+
+    private _createScene(): Scene {
         this._scene = new Scene(this._engine);
+
+        this._camera = new ArcRotateCamera(
+            "camera",
+            -Math.PI / 2,
+            -Math.PI / 2.5,
+            3,
+            new Vector3(0, 0, 0)
+        );
 
         this._light = new HemisphericLight(
             "light",
@@ -24,12 +44,9 @@ class App {
             this._scene
         );
 
+        this._box = MeshBuilder.CreateBox("box", {}, this._scene);
 
-
-        // run the main render loop
-        this._engine.runRenderLoop(() => {
-            this._scene.render();
-        });
+        return this._scene;
     }
 
     private _createCanvas(): HTMLCanvasElement {
