3b32154cb937b077c8d116ebab14cd904eb0e481
Robyn Drake <robin.drake.041@gmail.com>
2022-09-23 04:40:37 -0500

    Start of starting over

--- src/app.ts
+++ src/app.ts
@@ -4,63 +4,11 @@ import "@babylonjs/loaders/glTF";
 import {Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder } from "@babylonjs/core";
 
 class App {
-    // General Entire Application
-    private _scene: Scene;
-    private _canvas: HTMLCanvasElement;
-    private _engine: Engine;
-    private _light: HemisphericLight;
-    private _box: Mesh;
-    private _ground: Mesh;
-    private _camera: ArcRotateCamera;
-    
     constructor() {
-        this._canvas = this._createCanvas();
-
-        // initialize babylon scene and engine
-        this._engine = new Engine(this._canvas, true);
-        this._scene = this._createScene();
-
-        
-
-        // run the main render loop
-        this._engine.runRenderLoop(() => {
-            this._scene.render();
-        });
-    }
-
-    private _createScene(): Scene {
-        this._scene = new Scene(this._engine);
-
-        this._camera = new ArcRotateCamera(
-            "camera",
-            -Math.PI / 2,
-            -Math.PI / 2.5,
-            3,
-            new Vector3(0, 0, 0)
-        );
-
-        this._light = new HemisphericLight(
-            "light",
-            new Vector3(0, 1, 0),
-            this._scene
-        );
-
-        this._ground = MeshBuilder.CreateGround("ground", {width: 10, height: 10}, this._scene);
-
-        //this._box = MeshBuilder.CreateBox("box", {}, this._scene);
-        //this._box.position.y = 0.5;
-
-        return this._scene;
-    }
-
-    private _createCanvas(): HTMLCanvasElement {
-        this._canvas = document.createElement("canvas");
-        this._canvas.width = 800;
-        this._canvas.height = 600;
-        this._canvas.id = "gameCanvas";
-        document.body.appendChild(this._canvas);
-        return this._canvas;
-
+        var canvas = document.createElement("canvas");
+        canvas.style.width = 800;
+        canvas.style.height = 600;
+        canvas.id = "gameCanvas";
+        document.body.appendChild(canvas);
     }
 }
-new App();
