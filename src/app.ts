48d49ad762c14d1ee368af11bd93ade35b942f99
Robyn Drake <robin.drake.041@gmail.com>
2022-09-23 14:00:38 -0500

    tutorial code entered

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,31 @@ import {Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBui
 class App {
     constructor() {
         var canvas = document.createElement("canvas");
-        canvas.style.width = 800;
-        canvas.style.height = 600;
+        canvas.style.width = "800px";
+        canvas.style.height = "600px";
         canvas.id = "gameCanvas";
         document.body.appendChild(canvas);
+
+        var engine = new Engine(canvas, true);
+        var scene = new Scene(engine);
+
+        var camera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, Vector3.Zero(), scene);
+        camera.attachControl(canvas, true);
+        var light1 = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
+        var sphere = MeshBuilder.CreateSphere("sphere", {diameter: 1}, scene);
+
+        window.addEventListener("keydown", (ev) => {
+            if(ev.shiftKey && ev.ctrlKey && ev.altKey && ev.keyCode === 73) {
+                if(scene.debugLayer.isVisible())
+                    scene.debugLayer.hide();
+                else
+                    scene.debugLayer.show();
+            }
+        });
+
+        engine.runRenderLoop(() => {
+            scene.render();
+        });
+
     }
 }
