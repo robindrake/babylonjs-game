cf6b1b4416dd8f64a815b05090e4b3f534d8e53e
Robyn Drake <robin.drake.041@gmail.com>
2022-09-25 14:12:40 -0500

    further debugging to get express server working

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
-import express, { Application } from 'express';
+const express = require('express');
+const path = require('path');
 
-const app: Application = express();
+const app = express();
 
 const port: number = 3000;
 
 app.get('/', (_req, _res) => {
-    _res.sendFile("./index.html");
+    _res.sendFile( path.join(__dirname + "\index.html") );
 });
 
 app.listen(port, () => {
