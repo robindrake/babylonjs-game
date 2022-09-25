ae62bcfd4cdb3ac11d2ceac9cb7ba88bec30f889
Robyn Drake <robin.drake.041@gmail.com>
2022-09-25 14:33:09 -0500

    branch express works

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var port = 3000;
+app.use(express.static("public"));
+app.get('/', function (_req, _res) {
+    _res.sendFile(path.join("".concat(__dirname, "index.html")));
+});
+app.listen(port, function () {
+    console.log("Server started on ".concat(port));
+});
--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,10 @@ const app = express();
 
 const port: number = 3000;
 
+app.use(express.static("public"));
+
 app.get('/', (_req, _res) => {
-    _res.sendFile( path.join(__dirname + "\index.html") );
+    _res.sendFile( path.join(`${__dirname}\index.html`) );
 });
 
 app.listen(port, () => {
