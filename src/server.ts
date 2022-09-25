586a1a153c5f00bb4848e087e1a21aaf7df7373f
Robyn Drake <robin.drake.041@gmail.com>
2022-09-25 13:38:02 -0500

    further debugging for cyclic

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import express from 'express';
+import express, { Application } from 'express';
 
-const app: express.Application = express();
+const app: Application = express();
 
 const port: number = 3000;
 
