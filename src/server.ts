50c4b2b0778120e06d70bb5ff6352d783a1c2455
Robyn Drake <robin.drake.041@gmail.com>
2022-09-25 12:42:01 -0500

    server should be done...

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app: express.Application = express();
+
+const port: number = 3000;
+
+app.get('/', (_req, _res) => {
+    _res.sendFile("index.html");
+});
+
+app.listen(port, () => {
+    console.log(`Server started on ${port}`);
+});
