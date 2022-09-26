884be71f545ff3d9593ff57bd0017c5ffe147ea4
GabeeCoding <gabnology@gmail.com>
2022-09-26 13:37:11 +0100

    Webpack uses template from src and uses correct entry

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const appDirectory = fs.realpathSync(process.cwd());
 
 module.exports = {
-    entry: path.resolve(appDirectory, "src/app.ts"), //path to the main .ts file
+    entry: path.resolve(appDirectory, "src/game.ts"), //path to the main .ts file
     output: {
         filename: "js/bundleName.js", //name for the js file that is created/compiled in memory
         clean: true,
@@ -33,7 +33,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             inject: true,
-            template: path.resolve(appDirectory, "public/index.html"),
+            template: path.resolve(appDirectory, "src/index.html"),
         })
     ],
     mode: "development",
